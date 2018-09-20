636142789f22fd90b46643de0b1bc416a525ae79
Cao Anh Quan <aquancva@gmail.com>
2018-09-20 23:15:31 +0200

    add test result

--- src/RandomData.java
+++ src/RandomData.java
@@ -3,14 +3,20 @@ import java.util.Random;
 public class RandomData {
 	public static void main(String[] args) {
 		Random random = new Random();
-		
+
 		String[] items = { "book", "car", "cd", "dvd", "hat", "cake" };
-		int numberOfTrans = 20;
-		int maxNumberOfItemsPerTran = 10;
+		int numberOfTrans = 15;
+		int maxNumberOfItemsPerTran = 5;
 
 		for (int i = 0; i < numberOfTrans; i++) {
-			
-//			for (int j = 0; j < maxN)
+			int numberOfItems = 5 + random.nextInt(maxNumberOfItemsPerTran);
+			for (int j = 0; j < numberOfItems; j++) {
+				int randIdx = random.nextInt(items.length);
+				String item = items[randIdx];
+				int randInt = random.nextInt(5);
+				System.out.print(item + randIdx + " ");
+			}
+			System.out.print("\n");
 		}
 	}
 }
